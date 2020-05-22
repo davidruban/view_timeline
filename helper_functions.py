8c4fe151c3313af37205cf28fa470537c5ca043f
David Kislyak <kislyakdavid@gmail.com>
2020-05-22 02:49:52 -0700

    First commit on new public repo.

---
+++ helper_functions.py
@@ -0,0 +1,58 @@
+import tweepy, creds
+
+
+# Creates the twitter api connection and returns a tweepy
+# object after it's been authenticated
+def create_tweepy_object():
+    # Note: You will need to apply for a twitter developer
+    # account @https://developer.twitter.com/en to obtain these credentials
+    auth = tweepy.OAuthHandler(creds.consumer_key, creds.consumer_secret)
+    auth.set_access_token(creds.access_token, creds.access_token_secret)
+
+    # Rate limit aware authentication
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+
+    # Validate Credentials
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
+
+    # Return tweepy object
+    return api
+
+
+# Takes a list of member id's and adds them to a twitter list.
+def add_members(twitter_api, list_id, friends_ids):
+    # List holding members to be added to the twitter list
+    members_list = []
+
+    # Add each member into the list.
+    for i, account_id in enumerate(friends_ids):
+        # There is a limit of 5000 members on twitter lists.
+        # So you need to stop adding past that.
+        if i == 5000:
+            members_list = []
+            break
+        # The add_list_members() method has a maximum of 100 users
+        elif i % 100 == 0:
+            if i != 0:
+                twitter_api.add_list_members(list_id=list_id, user_id=members_list)
+                members_list = []
+            else:
+                members_list.append(account_id)
+        else:
+            members_list.append(account_id)
+
+    # Check to see if there are any friends left to add
+    if len(members_list) > 0:
+        twitter_api.add_list_members(list_id=list_id, user_id=members_list)
+
+    return True
+
+
+# Returns back a json object with the current accounts rate limits.
+def check_rate_limits(twitter_api):
+    return twitter_api.rate_limit_status()
---
+++ view_timeline.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from helper_functions import add_members, create_tweepy_object
+
+# Target twitter account
+twitter_handle = "ExploitDB"  # Ex: "github" (which is @github)
+timeline_date = datetime.today().strftime('%m/%d/%y')
+
+if __name__ == "__main__":
+    # Create twitter api connection
+    api = create_tweepy_object()
+
+    # Get target twitter handle's user object.
+    twitter_account = api.get_user(twitter_handle)
+
+    # Create list, and save it's ID
+    list_id = \
+        getattr(
+            api.create_list((twitter_handle + "'s " + " timeline"), "public")
+            , 'id')
+
+    # Get twitter friends account ID's
+    twitter_account_friends = api.friends_ids(twitter_handle)
+
+    # Add friends to timeline list
+    add_members(api, list_id, twitter_account_friends)
+
+    # Return list url for viewing the timeline
+    print('You may view @' + twitter_handle + "'s timeline at " +
+          'https://twitter.com/i/lists/' + str(list_id))
